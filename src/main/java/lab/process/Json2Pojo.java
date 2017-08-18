68f69c32c0cf7f86931b21a0fe0a3821bbcbcb63
Roger Viñas Alcon <roger.vinas@scmspain.com>
2017-08-18 10:26:27 +0200

    Simplify process

--- src/main/java/lab/process/Json2Pojo.java
+++ src/main/java/lab/process/Json2Pojo.java
@@ -2,16 +2,21 @@ package lab.process;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lab.helpers.ObjectMapperFactory;
 
-import java.io.IOException;
+import java.io.FileReader;
 import java.io.Reader;
 
 public class Json2Pojo {
 
-    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final ObjectMapper mapper = ObjectMapperFactory.create();
 
-    public static <T> T process(Reader json, Class<T> pojoClass) throws IOException {
-        return mapper.readValue(json,pojoClass);
+    public static <T> T process(String jsonResourceName, Class<T> pojoClass) throws Exception {
+        return process(new FileReader("src/main/resources/" + jsonResourceName), pojoClass);
+    }
+
+    public static <T> T process(Reader jsonReader, Class<T> pojoClass) throws Exception {
+        return mapper.readValue(jsonReader, pojoClass);
     }
 
 }
--- src/main/java/lab/process/PojoToJson.java
+++ src/main/java/lab/process/PojoToJson.java
@@ -2,15 +2,23 @@ package lab.process;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lab.helpers.ObjectMapperFactory;
 
-import java.io.IOException;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.Writer;
 
 public class PojoToJson {
 
-    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final ObjectMapper mapper = ObjectMapperFactory.create();
 
-    public static void process(Object instance, Writer writer) throws IOException {
-        mapper.writeValue(writer,instance);
+    public static void process(Object pojo, String jsonResourceName) throws Exception {
+        File jsonResourceFile = new File("src/main/resources/" + jsonResourceName);
+        jsonResourceFile.getParentFile().mkdirs();
+        process(pojo, new FileWriter(jsonResourceFile));
+    }
+
+    public static void process(Object pojo, Writer jsonWriter) throws Exception {
+        mapper.writeValue(jsonWriter, pojo);
     }
 }
